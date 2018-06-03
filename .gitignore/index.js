cb1de72858c934985721bcdf38f34b0bd4e2a4ce
CaroloffTV <39883429+CaroloffTV@users.noreply.github.com>
2018-06-03 19:53:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,26 +1,8 @@
 const Discord = require("discord.js");
-const low = require("lowdb")
-const FileSync = require("lowdb/adapters/FileSync")
-
-const adapter = new FileSync("database.json");
-const db = low(adapter);
-
-db.defaults({ histoires: []})
-.write()
-
-
-
 var bot = new Discord.Client();
-var prefix = ("/")
-var randnum = 0
-
-
-
-
-
-bot.on("ready" , () => {
-
-   console.log("Bot Ready !"); 
-});
-
-bot.login("NDMyOTY4NTE0OTY2NTE5ODI4.Da1NYA.zPWNG5X546Ez_XEHyXbPFVgwSeM");
+bot.on("ready" , function()  {
+    bot.user.setGame("ShowBot, !help");
+    console.log("Bot Ready !"); 
+ });
+ 
+ bot.login("NDMyOTY4NTE0OTY2NTE5ODI4.Da1NYA.zPWNG5X546Ez_XEHyXbPFVgwSeM");
