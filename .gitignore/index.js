332a7fe1a2f076fa81ecf105c57106eefe5cbf20
CaroloffTV <39883429+CaroloffTV@users.noreply.github.com>
2018-06-03 20:45:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,83 +12,10 @@ bot.on("message" , message => {
        message.reply("pong");
        console.log("ping pong");
     }
- if (!message.content.startsWith(prefix)) return;
- var args = message.content.substring(prefix.length).split(" ");
-
- switch (args[0].toLowerCase()){
-
-case "newstory":
-var value = message.content.substr(10); 
-var author = message.author.toString();
-var number = db.get('histoires').map('id').value();
-//var storyid = number + 1;
-console.log(value);
-message.reply("Ajout de l'histoire dans la base de donnée")
-db.get("histoires")
- .push({  story_value: value, story_author: author})
- .write();
-
-
-break;
-  
-case "kick":
-
-if (!message.channel.permissionsFor(message.member).hasPermissions("KICK_MEMBERS")){
-
-message.reply("Tu n'as pas le droit de kick ! ")
-}else{
-var memberkick = message.mentions.members.first();
-console.log(memberkick)
-console.log(message.guild.member(memberkick).kickable)
-if(!message.guild.member){
-message.reply("L'utilisateur n'existe pas ! ");
-}else{
-if(!message.guild.member(memberkick).kickable){
-	message.reply("Utilisateur impossible à kick ! ");
-}else{
-message.guild.member(memberkick).kick().then((member) => {
-	message.channel.send('${member.displayName} a été kick ! ');
-}).catch(() => {
-	message.channel.send("kick Refusé !")
-
-})
-}
-}
-}
-
-
-break;
-case "ban":
-
-if (!message.channel.permissionsFor(message.member).hasPermissions("BAN_MEMBERS")){
-
-message.reply("Tu n'as pas le droit de ban ! ")
-}else{
-var memberban = message.mentions.members.first();
-console.log(memberban)
-console.log(message.guild.member(memberban).bannable)
-if(!memberban){
-message.reply("L'utilisateur n'existe pas ! ");
-}else{
-if(!message.guild.member(memberban).bannable){
-	message.reply("Utilisateur impossible à ban ! ");
-}else{
-message.guild.member(memberban).ban().then((member) => {
-	message.channel.send('${member.displayName} a été ban ! ');
-}).catch(() => {
-	message.channel.send("ban Refusé !")
-
-})
-}
-}
-}
-
-
-break;
 
 
 
- }
+
  
 
     if (message.content === prefix + "help"){
